f6cebc653f5490a70560c496b81fa65120caee0c
Houssem Marzougui <marzouguih86@gmail.com>
2021-11-28 23:49:19 -0800

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -11,3 +13,9 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {"data": "Our Posts"}
+
+
+@app.post("/posts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
