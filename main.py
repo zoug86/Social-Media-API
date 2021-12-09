8cf0099d65cffd10be1e4ae6fe9de8af1fc5ce82
Houssem Marzougui <marzouguih86@gmail.com>
2021-12-08 16:14:45 -0800

    added CRUD functionality

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import FastAPI, HTTPException, status
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randint
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [
+    {"title": "title post 1", "content": "First post content", "id": 1},
+    {"title": "title post 2", "content": "Second post content", "id": 2},
+]
+
+
+def find_post(id):
+    for post in my_posts:
+        if post["id"] == id:
+            return post
+    return None
 
 
 @app.get("/")
@@ -12,10 +34,41 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "Our Posts"}
+    return {"data": my_posts}
 
 
-@app.post("/posts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id {id} was not found")
+    return {"post_detail": post}
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_posts(post: Post):
+    post_id = randint(0, 1000000)
+    post_dict = {"id": post_id, **post.dict()}
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+
+@app.put("/posts/{id}")
+def delete_post(id: int, new_post: Post):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id {id} was not found")
+    post.update(new_post.dict())
+    return {"post_updated": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id {id} was not found")
+    my_posts.remove(post)
+    return {"message": "post deleted successfully"}
